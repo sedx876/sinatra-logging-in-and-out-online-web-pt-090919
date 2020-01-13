7415976e1101ea62e9eb96eae2e13506a7cb63ec
Sharon Watkins <swatkins876@gmail.com>
2020-01-13 00:41:27 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,14 @@
 class Helpers < Sinatra::Base
 
   def self.current_user(session)
-   User.find_by(id: session[:user_id])
- end
+    if !!user = User.find_by(id: session[:user_id])
+      user
+    else
+      nil
+    end
+  end
 
- def self.is_logged_in?(session)
-   !!session[:user_id]
- end
+  def self.is_logged_in?(session)
+    !!session[:user_id] = self.current_user(session).id
+  end
 end
